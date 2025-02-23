d1cfc62a820136949ecd2d23126045db640a4c03
Austine Juma <austinejumah001@gmail.com>
2025-02-24 00:57:10 +0300

    use userContext in Profile.jsx

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react"
+import UserContext from "./UserContext"
+import UserContext from "./UserContext"
+
 export default function UserProfile(props){
+
+    const userData = useContext(UserContext)
     return (
         <div>
-            <h2>{props.name}</h2>
+            <h2>{userData.name}</h2>
             <p>Age: {props.age}</p>
             <p>Bio: {props.bio}</p>
         </div>
