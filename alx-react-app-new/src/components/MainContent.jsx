24b36052ab574a871b266e1960cc828167b3407e
Austine Juma <austinejumah001@gmail.com>
2025-02-23 16:53:58 +0300

    Inline Styling

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+export default function MainContent(){
+    return(
+        <main>
+            <p style={{color: 'darkred', fontWeight: 'bold'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
