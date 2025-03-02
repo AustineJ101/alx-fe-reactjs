355586ec65bdb297ff61e9f433bbe75b51a10107
AustineJ <austinejumah001@gmail.com>
2025-03-02 22:37:10 +0300

    Enhanced recipe sharing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,20 @@
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/add-recipe" element={<AddRecipeForm />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,15 @@
 import { create } from "zustand";
 
-const useRecipeStore = create((set) => ({
+const useRecipeStore = create((set, get) => ({
     recipes: [],
     addRecipe: (newRecipe) => set(state => ({recipes: [...state.recipes, newRecipe]})),
-    setRecipes: (recipes) => set({recipes})
+    setRecipes: (recipes) => set({recipes}),
+    deleteRecipe: (recipeId) => {
+        let newRecipe = get().recipes.filter(recipe => recipe.id !== recipeId);
+        set({recipes: newRecipe})
+    },
+    updateRecipe: (recipe) => set({recipes: recipe});
+
 }));
 
 export default useRecipeStore;
