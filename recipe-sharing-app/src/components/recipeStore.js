2bd1201e6143d6845907cbf84771e2406d091050
AustineJ <austinejumah001@gmail.com>
2025-03-02 23:06:15 +0300

    rearrange store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,6 +2,12 @@ import { create } from "zustand";
 
 const useRecipeStore = create((set, get) => ({
     recipes: [],
+    searchTerm: '',
+    setSearchTerm: (term) => set({ searchTerm: term }),
+    filteredRecipes: [],
+    filterRecipes: () => set(state => ({
+        filteredRecipes: state.recipes.filter(recipe => recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()))
+    })),
     addRecipe: (newRecipe) => set(state => ({recipes: [...state.recipes, newRecipe]})),
     setRecipes: (recipes) => set({recipes}),
     deleteRecipe: (recipeId) => {
@@ -10,7 +16,8 @@ const useRecipeStore = create((set, get) => ({
     },
     updateRecipe: (recipeId) => {
         let updatedRecipe = get().recipes.filter(recipe => recipe.id == recipeId)
-    }
+    },
+    
 
 }));
 
