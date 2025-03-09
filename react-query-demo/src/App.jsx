957ed83ff3224de641c28536a1f9c21872b79fed
Austine Juma <austinejumah001@gmail.com>
2025-03-09 20:43:01 +0300

    Delete PostsComponent

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,11 +1,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import PostsComponent from "./components/PostsComponent";
 const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <QueryClientProvider client={queryClient} >
-
+      <PostsComponent />
     </QueryClientProvider>
    
   )
