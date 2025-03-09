3b90709c4fa3074312b43cbb788a9eced0b66ec2
Austine Juma <austinejumah001@gmail.com>
2025-03-09 19:32:44 +0300

    Correct import

--- form-handling-react/src/main.jsx
+++ form-handling-react/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import RegistrationForm from './components/RegistrationForm.jsx'
+import FormikForm from './components/forimikForm.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+    <RegistrationForm />
   </StrictMode>,
 )
