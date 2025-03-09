902a5538a1bd8b08918b7ac4dfd665e0b87cf179
Austine Juma <austinejumah001@gmail.com>
2025-03-09 20:43:48 +0300

    Include PostsComponents in App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,44 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import PostsComponent from "./components/PostsComponent";
 const queryClient = new QueryClient();
+
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+
+function PostsComponent(){
+    const {data: posts, isLoading, error} = useQuery({
+        queryKey: ["posts"],
+        queryFn: async () => {
+            const response = await axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+            return response.data;
+        }
+    })
+
+    const queryClient = useQueryClient();
+
+    if(isLoading){
+        return <p>Loading...</p>
+    }
+
+    if(error){
+        return <p>Error Loading Posts</p>
+    }
+
+    return (
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                {posts.map(post => (
+                    <li key={post.id}>{post.body}</li>
+                ))}
+            </ul>
+
+            <button onClick={() => {
+                queryClient.invalidateQueries(["posts"])
+            }}>Refetch</button>
+        </div>
+    )
+}
+
 
 function App() {
 
