a965fe9e818ea5f6cf3a05a5f8ab691cf110b921
Austine Juma <austinejumah001@gmail.com>
2025-03-09 19:53:20 +0300

    Rename file to .js

--- form-handling-react/src/main.jsx
+++ form-handling-react/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import RegistrationForm from './components/RegistrationForm.jsx'
-import FormikForm from './components/forimikForm.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RegistrationForm />
