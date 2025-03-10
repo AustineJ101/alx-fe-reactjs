4ff5e4ec331ada43eccf5d0334f999f6ec827c53
Austine Juma <austinejumah001@gmail.com>
2025-03-10 16:45:46 +0300

    Initial commit

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,22 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./components/Home"
-import Profile from "./components/Profile"
-import ProfileDetails from "./components/ProfileDetails"
-import ProfileSettings from "./components/ProfileSettings"
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/profile" element={<Profile />}>
-          <Route path="details/:user" element={<ProfileDetails />}/>
-          <Route path="settings" element={<ProfileSettings />}/>
-        </Route>
-      </Routes>
-    </Router>
-    
+    <>
+     <h1>Advanced React Routing</h1>
+    </>
   )
 }
 
