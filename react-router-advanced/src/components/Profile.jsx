b50e1f97c68db5d4e06a993f989b79f1b77b37d9
Austine Juma <austinejumah001@gmail.com>
2025-03-10 17:07:48 +0300

    Modify Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,24 +1,30 @@
-import { Link, Outlet } from "react-router-dom"
-function Profile(){
-    return(
+import { Link, Routes, Route, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
+
+function Profile() {
+    return (
         <div>
             <h1>This is the Profile Page</h1>
-            <nav style={{display: "flex", gap: "10px"}}>
+            <nav style={{ display: "flex", gap: "10px" }}>
                 <Link to="settings">Settings</Link>
             </nav>
 
-            
-            <ul style={{display: "flex", flexDirection: "column", gap: "5px"}}>
-                <Link to="/profile/details/Austine">Austine</Link>
-                <Link to="/profile/details/Jamie">Jamie</Link>
-                <Link to="/profile/details/Ghost">Ghost</Link>
-                <Link to="/profile/details/Tommie">Tommie</Link>
+            <ul style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+                <Link to="details/Austine">Austine</Link>
+                <Link to="details/Jamie">Jamie</Link>
+                <Link to="details/Ghost">Ghost</Link>
+                <Link to="details/Tommie">Tommie</Link>
             </ul>
-            
-           
+
+            <Routes>
+                <Route path="details/:user" element={<ProfileDetails />} />
+                <Route path="settings" element={<ProfileSettings />} />
+            </Routes>
+
             <Outlet />
         </div>
-    )
+    );
 }
 
 export default Profile;
