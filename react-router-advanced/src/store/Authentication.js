4220b4e9ea12d96f01ab84ec1e9a97aff990949d
Austine Juma <austinejumah001@gmail.com>
2025-03-10 17:26:51 +0300

    create a zustand store

---
+++ react-router-advanced/src/store/Authentication.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+const useAuth = create((set) => ({
+    isAuthenticated: false,
+
+    login: () => set({isAuthenticated: true}),
+
+    logout: () => set({isAuthenticated: false})
+}));
+
+export default useAuth;
