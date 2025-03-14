8c0458860d7b6804ba4557603145d438bf21e8c3
Austine Juma <austinejumah001@gmail.com>
2025-03-14 17:00:51 +0300

    Set text-lg for heading on sm:

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ function UserProfile(){
     return(
         <div className="bg-gray-100 md:p-8 sm:p-4 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
             <img src="https://via.placeholder.com/150" alt="User" className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto"/>
-            <h1 className="md:text-xl sm:text-sm text-blue-800 my-4">John Doe</h1>
+            <h1 className="md:text-xl sm:text-lg text-blue-800 my-4">John Doe</h1>
             <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     );
