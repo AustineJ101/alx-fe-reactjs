fa751652e610eeec3b04b6f395eda9ff05758741
Austine Juma <austinejumah001@gmail.com>
2025-03-14 15:34:34 +0300

    set up tailwind css in react

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+    <button className="text-white font-semibold bg-pink-700 px-4 py-2 rounded-lg m-10 hover:bg-white hover:text-pink-700 hover:border-2 hover:border-pink-700 transition" onClick={() => {console.log("Button Clicked")}}>Login</button>
+  )
+}
+
+export default App
