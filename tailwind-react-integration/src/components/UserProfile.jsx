8dd13cc22f1614104d39a828679ed3be37be23fa
Austine Juma <austinejumah001@gmail.com>
2025-03-14 16:58:58 +0300

    Responsive design implementation

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,8 +1,8 @@
 function UserProfile(){
     return(
-        <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-            <img src="https://via.placeholder.com/150" alt="User" className="w-36 h-36 rounded-full mx-auto"/>
-            <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
+        <div className="bg-gray-100 md:p-8 sm:p-4 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+            <img src="https://via.placeholder.com/150" alt="User" className="md:w-36 md:h-36 sm:w-24 sm:h-24 rounded-full mx-auto"/>
+            <h1 className="md:text-xl sm:text-sm text-blue-800 my-4">John Doe</h1>
             <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     );
