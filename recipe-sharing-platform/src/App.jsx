df972838210a20b31ba621b8c3474e569cd0fb9e
Austine Juma <austinejumah001@gmail.com>
2025-03-18 21:36:49 +0300

    Create HomePage

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,10 +1,12 @@
+import HomePage from "./components/HomePage"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-blue-500">Recipe Sharing App</h1>
-    </>
+   <>
+   <h1 className="text-center text-2xl font-bold underline text-pink-600">Recipe Sharing Platform</h1>
+    <HomePage />
+   </>
   )
 }
 
---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import recipesData from "../data.json"
+
+function HomePage(){
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        setRecipes(recipesData)
+    }, []);
+
+    return(
+        <div className = "grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 p-6">
+            {recipes.map(recipe => (
+                <div key={recipe.id} className="border p-6 rounded-lg bg-gray-100 cursor-pointer hover:shadow transition">
+                    <h1 className="text-xl text-pink-600 font-semibold mb-2">{recipe.title}</h1>
+                    <hr className="mb-2 border-pink-600"/>
+                    <img src={recipe.image}  className="mb-2"/>
+                    <p className="text-gray-600">{recipe.summary}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default HomePage;
