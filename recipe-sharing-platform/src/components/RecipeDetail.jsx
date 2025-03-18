dece99c5fb1ff1fb79423d0f7f7b04ec91115d88
Austine Juma <austinejumah001@gmail.com>
2025-03-18 22:59:05 +0300

    Add instructions and ingredients

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,8 +1,20 @@
 import { useParams } from "react-router-dom";
+import { use, useEffect, useState } from "react";
 import recipeData from "../data.json"
 
 function RecipeDetail(){
+    const[ingredients, setIngredients] = useState("");
+    const [instructions, setInstructions] = useState("");
     const {id} = useParams();
+    useEffect(() =>{
+        if(id == 1){
+            setIngredients("Ingredients for 1")
+            setInstructions("Instructions for 1")
+        }else{
+            setIngredients("Ingredients for 2")
+            setInstructions("Instructions for 2")
+        }
+    }, [])
     const recipe = recipeData.filter(recipe => recipe.id == id)
     
     return(
@@ -11,7 +23,9 @@ function RecipeDetail(){
             <hr className="my-2 border-pink-600"/>
             <img src={recipe[0].image}/>
             <h2 className="font-semibold text-lg sm:text-xl mb-2">Instructions</h2>
-            <p className="text-gray-600">{recipe[0].summary}</p>
+            <p className="text-gray-600">{instructions}</p>
+            <h2 className="font-semibold text-lg sm:text-xl mb-2">Ingredients</h2>
+            <p className="text-gray-600">{ingredients}</p>
 
         </div>
     )
